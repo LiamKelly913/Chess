d809354f6f47248435ff06518e02bd876c70db01
Liam Kelly <liamkelly913@gmail.com>
2017-03-26 18:04:41 -0400

    Added UI

--- Chess/Board.swift
+++ Chess/Board.swift
@@ -19,24 +19,21 @@ class Board {
      */
     
     struct Piece {
-        var type:String = ""
-        var color:String = ""
+        var type:String = String()
+        var color:String = String()
         var button:UIButton = UIButton()
-        var currentPos:[Int] = []
+        var currentPos:[Int] = [Int]()
     }
     
     //TODO: Add current space to the piece in question when generating baord
     struct Space {
-        var position:String = ""
+        var position:String = String()
         var piece:Piece
         var location:CGPoint
-        
-        func isOccupied() -> Bool {
-            if(piece.type == "") { return false } else { return true }
-        }
+        var isOccupied = false
         
         func getColor() -> String {
-            if self.isOccupied() {
+            if self.isOccupied {
                 return self.piece.color
             } else {
                 return "Unnocupied"
@@ -44,12 +41,13 @@ class Board {
         }
     }
     
-    var blackPieceLocations:[[Int]] = [[]]
-    var whitePieceLocations:[[Int]] = [[]]
+    var blackPieceLocations:[[Int]] = [[Int]]()
+    var whitePieceLocations:[[Int]] = [[Int]]()
+    var deadWhite:[Piece] = [Piece]()
+    var deadBlack:[Piece] = [Piece]()
     
     
-    
-    var board = Array(repeating: Array(repeating: Space(position: "", piece: Piece(), location: CGPoint(x: 0, y: 0)), count: 8), count: 8)
+    var board = Array(repeating: Array(repeating: Space(position: "", piece: Piece(), location: CGPoint(x: 0, y: 0), isOccupied: false), count: 8), count: 8)
     let columns = ["a","b","c","d","e","f","g","h"]
     
     let backLine = ["Rook","Night","Bishop","Queen","King","Bishop","Night","Rook"]
@@ -78,29 +76,34 @@ class Board {
     // Set up beginning state of board
     func setupPieces() {
         var col = 0
-        var backIndex = 7
         for _ in 1...8 {
             board[0][col].piece.type = backLine[col]
             blackPieceLocations.append([0,col])
             board[0][col].piece.currentPos = [0, col]
+            board[0][col].isOccupied = true
             board[0][col].piece.color = "Black"
+            
             board[1][col].piece.type = "Pawn"
             blackPieceLocations.append([1,col])
-            board[0][col].piece.currentPos = [0, col]
+            board[1][col].piece.currentPos = [1, col]
+            board[1][col].isOccupied = true
             board[1][col].piece.color = "Black"
             
             
-            board[7][backIndex].piece.type = backLine[col]
+            board[7][col].piece.type = backLine[col]
             whitePieceLocations.append([7,col])
-            board[7][backIndex].piece.currentPos = [7, col]
-            board[7][backIndex].piece.color = "White"
+            board[7][col].piece.currentPos = [7, col]
+            board[7][col].isOccupied = true
+            board[7][col].piece.color = "White"
+            
+            
             board[6][col].piece.type = "Pawn"
             whitePieceLocations.append([6,col])
             board[6][col].piece.currentPos = [6, col]
+            board[6][col].isOccupied = true
             board[6][col].piece.color = "White"
             
             col+=1
-            backIndex-=1
         }
     }
     
@@ -131,23 +134,38 @@ class Board {
             for space in spot {
                 if space.location == location {
                     piece = space.piece
+                    break
                 }
             }
         }
         return piece
     }
     
-    
+    func getRCforXY(location:CGPoint) -> [Int] {
+        var RC:[Int] = [Int]()
+        var row = 0
+        var col = 0
+        for spot in board {
+            for space in spot {
+                if space.location == location {
+                    RC = [row,col]
+                }
+                col+=1
+            }
+            col = 0
+            row+=1
+        }
+        return RC
+    }
     
     
     func getXYForPos(pos:[Int]) -> CGPoint {
-        var xy:CGPoint?
-        let test = pos
+        var xy:CGPoint!
         var currRow:Int = 0
         var col:Int = 0
         for row in board {
             for space in row {
-                if ([currRow,col] == test) {
+                if ([currRow,col] == pos) {
                     xy = space.location
                     break
                 }
@@ -156,8 +174,11 @@ class Board {
             col = 0
             currRow+=1
         }
-        return xy!
-        
+        if(xy != nil) {
+            return xy
+        } else {
+            return CGPoint(x: CGFloat(1), y: CGFloat(1))
+        }
     }
     
     func printPositions() {
@@ -182,7 +203,7 @@ class Board {
         var col = 0
         for _ in 1 ... 8 {
             for _ in 1 ... 8 {
-                if (board[row][col].piece.type == "") {
+                if (board[row][col].isOccupied == false) {
                     print("• ", terminator: "")
                 } else {
                     print(String(board[row][col].piece.type.characters.prefix(1)) + " ", terminator: "")
@@ -195,13 +216,7 @@ class Board {
         }
         print()
     }
-    
 
-    // Update board array for when a piece has been played
-    func playMove() {
-     
-        //TODO: update list of color locations
-    }
     
     func printBoardWithPossibleMoves(playableSpaces:[[Int]]) {
         print()
@@ -220,7 +235,7 @@ class Board {
                     index+=1
                 }
                 index = 0
-                if (board[row][col].piece.type == "" && found == false) {
+                if (board[row][col].isOccupied && found == false) {
                     print("• ", terminator: "")
                 } else if (found == false) {
                     print(String(board[row][col].piece.type.characters.prefix(1)) + " ", terminator: "")
@@ -235,30 +250,37 @@ class Board {
         print()
     }
     
+    // Moves a selected piece from its current spot to a new spot
+    func movePiece(piece:Piece, to:[Int]) {
+        var newPiece = piece
+        var oldPos = piece.currentPos
+        let row = to[0]
+        let col = to[1]
+        
+        // this can only happen if a piece is being taken
+        if(board[row][col].isOccupied) {
+            let pieceToRemove = board[row][col].piece
+            if(piece.color == "Black") {
+                deadWhite.append(pieceToRemove)
+            } else {
+                deadBlack.append(pieceToRemove)
+            }
+        }
+        newPiece.currentPos = to
+        board[row][col].piece = newPiece
+        board[row][col].isOccupied = true
+        board[oldPos[0]][oldPos[1]].isOccupied = false
+        
+        if(piece.color == "Black") {
+            blackPieceLocations.append(to)
+            blackPieceLocations = blackPieceLocations.filter() {$0 != oldPos}
+        } else {
+            whitePieceLocations.append(to)
+            whitePieceLocations = whitePieceLocations.filter() {$0 != oldPos}
+        }
+    }
+    
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
